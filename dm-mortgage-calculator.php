7871445fb1fc759d0d2032dca8f16098d25dd754
derekmorash <derek.morash@gmail.com>
2015-07-20 09:16:01 -0300

    Plugin Start

---
+++ dm-mortgage-calculator.php
@@ -0,0 +1,13 @@
+<?php
+/*
+Plugin Name: DM Mortgage Calculator
+Plugin URI:  dm-mortgage-calculator.php
+Description: This describes my plugin in a short sentence
+Version:     0.1
+Author:      Derek Morash
+Author URI:  http://derekmorash.com/
+License:     GPL2
+License URI: https://www.gnu.org/licenses/gpl-2.0.html
+Domain Path: /languages
+Text Domain: dm-mortgage-calculator
+*/
